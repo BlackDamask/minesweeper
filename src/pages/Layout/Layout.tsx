f45a0a53856dd5c6b74a79035750ad4994386317
BlackDamask <tupamaxim6@gmail.com>
2024-10-16 20:18:31 +0200

    .

--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -5,25 +5,35 @@ export default function Layout(){
         <div className = 'container columns-2' >
             <Card maxW='sm'>
                 <CardHeader>
-                    
+                    Single player
                 </CardHeader>
                 <CardBody className='bg-slate-400'>
                     <Image
-                    src='https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
+                    src='./singleplayer.png'
                     alt='Green double couch with wooden legs'
                     borderRadius='lg'
                     />
                 </CardBody>
+                <CardFooter>
+
+                </CardFooter>
             </Card>     
-            <Card maxW='sm' className="bg-slate-100 ">
-                <CardBody>
+            <Card maxW='sm'>
+                <CardHeader>
+                    Saper Battle
+                </CardHeader>
+                <CardBody className='bg-slate-400'>
                     <Image
-                    src='https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
+                    src='./singleplayer.png'
                     alt='Green double couch with wooden legs'
                     borderRadius='lg'
                     />
                 </CardBody>
+                <CardFooter>
+                    
+                </CardFooter>
             </Card>           
         </div>
+        
     )
 }
