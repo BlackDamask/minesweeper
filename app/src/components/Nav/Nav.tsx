59f3df5364d01e4234dfbee5bd3949fe15dced06
BlackDamask <tupamaxim6@gmail.com>
2025-03-24 21:58:06 +0200

    responsiveness

--- app/src/components/Nav/Nav.tsx
+++ app/src/components/Nav/Nav.tsx
@@ -30,7 +30,7 @@ import ChangeUsernameModal from "../Modals/ChangeUsernameModal";
 export default function Nav() {
     const auth = useContext(AuthContext);
     const [isHovered, setIsHovered] = useState(false);
-    const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+    const [hoveredIndex, setHoveredIndex] = useState<number | null>(1);
     const hoverTimeout = useRef<NodeJS.Timeout | null>(null); 
     const [isSmallScreen, setIsSmallScreen] = useState(false);
     const [isClicked, setIsClicked] = useState(false);
@@ -80,6 +80,9 @@ export default function Nav() {
         const handleClickOutside = (event: MouseEvent) => {
             if (optionsBoxRef.current && !optionsBoxRef.current.contains(event.target as Node)) {
                 setIsClicked(false); // Close the options if clicked outside
+            }
+            else if(isSmallScreen){
+                setIsClicked(false);
             }
         };
 
@@ -147,144 +150,128 @@ export default function Nav() {
         }, 200); 
     };
 
-    if(isSmallScreen)
         return (
-            <Box className="absolute top-0 left-0 w-fit h-fit p-2">
-                <IconButton
-                    icon={<HamburgerIcon />}
-                    aria-label="Menu"
-                    variant="ghost"
-                    colorScheme="white "
-                    fontSize={'2xl'}
-                    className="text-white absolute"
-                />
-            </Box>
-        );
-
-    return (
-        <>
-            <div className="bg-slate-950 fixed left-0 h-screen w-20 flex flex-col items-center justify-between">
-                <Link to="/">
-                    <Box
-                        width={"100%"}
-                        height={"5em"}
-                        className="flex py-[0.5em] justify-center items-center hover:bg-gray-950"
-                    >
-                        <Image
-                            className="self-left w-[3.3em]"
-                            src="./logo-shape.png"
-                            alt="Return"
-                            borderRadius="lg"
-                        />
-                    </Box>
-                </Link>
-                <div>
-                    {buttons.map((button, index) => (
-                        <Box
-                            key={index}
-                            width={"100%"}
-                            height={"6em"}
-                            className="flex py-[0.5em] justify-center items-center hover:bg-gray-950"
-                            onMouseEnter={() => handleMouseEnterButton(index)}
-                            onMouseLeave={handleMouseLeaveButton}
-                            onClick={() => handleClickButton(index)}  // Added single-click behavior
-                        >
-                            <Image
-                                className="self-left w-[3.3em] m-2"
-                                src={button.imagePath}
-                                alt={button.alt}
-                                borderRadius="lg"
-                            />
-                        </Box>
-                    ))}
-                </div>
-                <div className="flex flex-col space-y-4 mb-10 items-center">
-                    {auth?.isLoggedIn 
-                        ? (
-                            <Popover placement="right">
-                                <PopoverTrigger>
-                                    <Avatar name={auth.user?.userName} />
-                                </PopoverTrigger>
-                                <PopoverContent width={"56"} borderWidth={3}>
-                                    <PopoverBody padding={0}>
-                                        <Box 
-                                            className="h-[2.5em] text-xl flex text-gray-100 items-center pl-3 filter brightness-100 hover:brightness-75 bg-slate-900"
-                                            onClick={onChangeUsernameOpen}
-                                        >
-                                            Change Username
-                                        </Box>
-                                        <Box 
-                                            className="h-[2.5em] text-xl flex text-gray-100 items-center pl-3 filter brightness-100 hover:brightness-75 bg-slate-900"
-                                            onClick={onLogoutOpen}
-                                        >
-                                            Log out
-                                        </Box>
-                                    </PopoverBody>
-                                </PopoverContent>
-                            </Popover>
-                        ) : (
-                            <div>
+                <>
+                    <div className="bg-slate-950 fixed left-0 h-screen w-14 sm:w-20 flex flex-col items-center justify-between">
+                        <Link to="/">
+                            <Box
+                                width="100%"
+                                height="5em"
+                                className="flex py-[0.5em] justify-center items-center hover:bg-gray-950"
+                            >
+                                <Image
+                                    className="self-left w-[3.3em]"
+                                    src="./logo-shape.png"
+                                    alt="Return"
+                                    borderRadius="lg"
+                                />
+                            </Box>
+                        </Link>
+                        <div>
+                            {buttons.map((button, index) => (
                                 <Box
-                                    width={"100%"}
-                                    height={"5em"}
-                                    className="flex fill-slate-300 hover:fill-white justify-center items-center hover:bg-gray-950"
-                                    onClick={onRegisterOpen}
+                                    key={index}
+                                    width="100%"
+                                    height="6em"
+                                    className="flex py-[0.5em] justify-center items-center hover:bg-gray-950"
+                                    onMouseEnter={() => handleMouseEnterButton(index)}
+                                    onMouseLeave={handleMouseLeaveButton}
+                                    onClick={() => handleClickButton(index)}
                                 >
-                                    <RegisterButton className="self-left fill-slate-300 hover:fill-white h-[4em] w-[2.5em]" />
+                                    <Image
+                                        className="self-left w-[3.3em] m-2"
+                                        src={button.imagePath}
+                                        alt={button.alt}
+                                        borderRadius="lg"
+                                    />
                                 </Box>
-                                <Box
-                                    width={"100%"}
-                                    height={"5em"}
-                                    className="flex py-[0.5em] fill-slate-300 hover:fill-white justify-center items-center hover:bg-gray-950"
-                                    onClick={onLoginOpen}
-                                >
-                                    <div className="flex justify-center w-16 bg-green-700 hover:bg-green-800 rounded-lg border-b-[3px] border-green-900">
-                                        <LoginButton className="self-left fill-slate-300 hover:fill-white h-[3em] w-[2.5em] m-2" />
-                                    </div>
-                                </Box>
-                            </div>
-                        )
-                    }
-                </div>
-            </div>
-
-            {((isHovered || isClicked) && hoveredIndex !== null && buttons[hoveredIndex]?.options) && (
-                <Box
-                    ref={optionsBoxRef}  // Add ref to track options box
-                    className="fixed left-20 w-80 bg-gray-950 h-screen z-50"
-                    onMouseEnter={handleMouseEnterOptionsBox}
-                    onMouseLeave={handleMouseLeaveOptionsBox}
-                >
-                    {buttons[hoveredIndex].options!.map((option, idx) => (
+                            ))}
+                        </div>
+                        <div className="flex flex-col space-y-4 mb-10 items-center">
+                            {auth?.isLoggedIn ? (
+                                <Popover placement="right">
+                                    <PopoverTrigger>
+                                        <Avatar name={auth.user?.userName} />
+                                    </PopoverTrigger>
+                                    <PopoverContent width="56" borderWidth={3}>
+                                        <PopoverBody padding={0}>
+                                            <Box
+                                                className="h-[2.5em] text-xl flex text-gray-100 items-center pl-3 filter brightness-100 hover:brightness-75 bg-slate-900"
+                                                onClick={onChangeUsernameOpen}
+                                            >
+                                                Change Username
+                                            </Box>
+                                            <Box
+                                                className="h-[2.5em] text-xl flex text-gray-100 items-center pl-3 filter brightness-100 hover:brightness-75 bg-slate-900"
+                                                onClick={onLogoutOpen}
+                                            >
+                                                Log out
+                                            </Box>
+                                        </PopoverBody>
+                                    </PopoverContent>
+                                </Popover>
+                            ) : (
+                                <div>
+                                    <Box
+                                        width="100%"
+                                        height="5em"
+                                        className="flex fill-slate-300 hover:fill-white justify-center items-center hover:bg-gray-950"
+                                        onClick={onRegisterOpen}
+                                    >
+                                        <RegisterButton className="self-left fill-slate-300 hover:fill-white h-[4em] w-[2.5em]" />
+                                    </Box>
+                                    <Box
+                                        width="100%"
+                                        height="5em"
+                                        className="flex py-[0.5em] fill-slate-300 hover:fill-white justify-center items-center hover:bg-gray-950"
+                                        onClick={onLoginOpen}
+                                    >
+                                        <div className="flex justify-center w-full bg-green-700 hover:bg-green-800 rounded-lg border-b-[3px] border-green-900">
+                                            <LoginButton className="self-left fill-slate-300 hover:fill-white h-[3em] w-[2.5em] m-2" />
+                                        </div>
+                                    </Box>
+                                </div>
+                            )}
+                        </div>
+                    </div>
+        
+                    {((isHovered || isClicked) && hoveredIndex !== null && buttons[hoveredIndex]?.options)  && (
                         <Box
-                            key={idx}
-                            width={"100%"}
-                            height={"5em"}
-                            className="flex items-center pl-5 text-gray-300 hover:text-white hover:bg-slate-950 font-bold"
+                            ref={optionsBoxRef}
+                            className="fixed left-20 w-80 bg-gray-950 h-screen z-50"
+                            onMouseEnter={handleMouseEnterOptionsBox}
+                            onMouseLeave={handleMouseLeaveOptionsBox}
                         >
-                            {option.alt}
+                            { buttons[hoveredIndex]?.options?.map((option, idx) => (
+                                <Box
+                                    key={idx}
+                                    width="100%"
+                                    height="5em"
+                                    className="flex items-center pl-5 text-gray-300 hover:text-white hover:bg-slate-950 font-bold"
+                                >
+                                    {option.alt}
+                                </Box>
+                            ))}
                         </Box>
-                    ))}
-                </Box>
-            )}
-
-            <LoginModal isOpen={isLoginOpen} onClose={onLoginClose} />
-            <RegisterModal isOpen={isRegisterOpen} onClose={onRegisterClose} />
-            <ChangeUsernameModal isOpen={isChangeUsernameOpen} onClose={onChangeUsernameClose} />
-            <Modal isCentered isOpen={isLogoutOpen} onClose={onLoginClose}>
-                <ModalOverlay />
-                <ModalContent>
-                    <ModalHeader>Log out</ModalHeader>
-                    <ModalCloseButton onClick={onLogoutClose} />
-                    <ModalBody>
-                        <p>Do you want to log out?</p>
-                    </ModalBody>
-                    <ModalFooter>
-                        <Button onClick={auth?.logout} marginRight={4}>Logout</Button>
-                        <Button onClick={onLogoutClose}>Close</Button>
-                    </ModalFooter>
-                </ModalContent>
-            </Modal>
-        </>
-    );
+                    )}
+        
+                    <LoginModal isOpen={isLoginOpen} onClose={onLoginClose} />
+                    <RegisterModal isOpen={isRegisterOpen} onClose={onRegisterClose} />
+                    <ChangeUsernameModal isOpen={isChangeUsernameOpen} onClose={onChangeUsernameClose} />
+                    <Modal isCentered isOpen={isLogoutOpen} onClose={onLoginClose}>
+                        <ModalOverlay />
+                        <ModalContent>
+                            <ModalHeader>Log out</ModalHeader>
+                            <ModalCloseButton onClick={onLogoutClose} />
+                            <ModalBody>
+                                <p>Do you want to log out?</p>
+                            </ModalBody>
+                            <ModalFooter>
+                                <Button onClick={auth?.logout} marginRight={4}>Logout</Button>
+                                <Button onClick={onLogoutClose}>Close</Button>
+                            </ModalFooter>
+                        </ModalContent>
+                    </Modal>
+                </>
+            );
 }
--- app/src/pages/Layout/Layout.tsx
+++ app/src/pages/Layout/Layout.tsx
@@ -27,40 +27,40 @@ export default function Layout() {
       <Nav/>
       <div className="w-[100%]">
         
-        <h1 className='ml-14 sm:ml-24 my-6 text-3xl text-gray-300'>
+        <h1 className='ml-16 sm:ml-24 my-6 text-3xl text-gray-300'>
           Start a new game
         </h1>
         
         <div className='flex flex-col space-y-5'>
           <Link to="/single">
-            <div className='flex ml-5 sm:ml-24 h-24 w-full lg:w-[80%] bg-green-700 hover:bg-green-800 rounded-lg border-b-[3px] border-green-900  cursor-pointer'>
+            <div className='flex  ml-14 sm:ml-24 h-24 w-[calc(100%-40px)] lg:w-[80%] bg-green-700 hover:bg-green-800 rounded-lg border-b-[3px] border-green-900  cursor-pointer'>
               <div className='w-1/3 sm:w-1/5 h-full p-3 '>
                 <img alt='' src="./bomb-shape.png" className='h-full aspect-square' />
               </div>
               <div className='w-4/5 flex flex-col p-4  '>
-                <h1 className='text-2xl text-white'>Single Player</h1>
-                <span className='flex text-white cursor-pointer'><p><u>Beginner 9x9</u> <u>Indermediate 16x16</u> <u>Expert 30x16</u></p> </span>
+                <h1 className='text-xl sm:text-2xl text-white'>Single Player</h1>
+                <span className='flex text-white cursor-pointer text-sm sm:text-base'><p><u>Beginner 9x9</u> <u>Indermediate 16x16</u> <u>Expert 30x16</u></p> </span>
               </div>
             </div>
           </Link>
-          <div className='flex ml-5 sm:ml-24 h-24 w-full lg:w-[80%] rounded-lg text-white bg-[#1072d6] hover:bg-[#0d5bab] border-b-[3px] border-[#0d5bab] cursor-pointer'
+          <div className='flex  ml-14 sm:ml-24 h-24 w-[calc(100%-40px)] lg:w-[80%] rounded-lg text-white bg-[#1072d6] hover:bg-[#0d5bab] border-b-[3px] border-[#0d5bab] cursor-pointer'
             onClick={handlePvpButtonClick}>
             <div className='w-1/3 sm:w-1/5 h-full p-3 '>
               <img alt='' src="./shovel-pvp.svg" className='h-full aspect-square' />
             </div>
             <div className='w-4/5 flex flex-col p-4  '>
-              <h1 className='text-2xl'>PvP</h1>
-              <span className='flex cursor-pointer'><p><u>Beginner 9x9</u> <u>Indermediate 16x16</u> <u>Expert 30x16</u></p> </span>
+              <h1 className='text-lg sm:text-2xl'>PvP</h1>
+              <span className='flex cursor-pointer text-sm sm:text-base'><p><u>Beginner 9x9</u> <u>Indermediate 16x16</u> <u>Expert 30x16</u></p> </span>
             </div>
           </div>
-          <div className="flex ml-5 sm:ml-24 h-24 w-full lg:w-[80%] bg-fuchsia-700 hover:bg-fuchsia-800 rounded-lg border-b-[3px] border-fuchsia-900 cursor-pointer">
+          <div className="flex  ml-14 sm:ml-24 h-24 w-[calc(100%-40px)] lg:w-[80%] bg-fuchsia-700 hover:bg-fuchsia-800 rounded-lg border-b-[3px] border-fuchsia-900 cursor-pointer">
 
             <div className='w-1/3 sm:w-1/5 h-full p-3 '>
               <img alt='' src="./coop-game.png" className='h-full aspect-square' />
             </div>
             <div className='w-4/5 flex flex-col p-4  '>
-              <h1 className='text-2xl text-white'>Cooperative game</h1>
-              <span className='flex text-white cursor-pointer'><p><u>Beginner 9x9</u> <u>Indermediate 16x16</u> <u>Expert 30x16</u></p> </span>
+              <h1 className='text-2xl text-white text-lg sm:text-2xl'>Cooperative game</h1>
+              <span className='flex text-white cursor-pointer text-sm sm:text-base'> <p><u>Beginner 9x9</u> <u>Indermediate 16x16</u> <u>Expert 30x16</u></p> </span>
             </div>
           </div>
         </div>
