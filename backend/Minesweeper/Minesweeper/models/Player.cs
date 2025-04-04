b9edee7c94034fccac46a8c09886c038ae39f1e2
BlackDamask <tupamaxim6@gmail.com>
2025-04-04 12:56:54 +0200

    .

--- backend/Minesweeper/Minesweeper/models/Player.cs
+++ backend/Minesweeper/Minesweeper/models/Player.cs
@@ -4,6 +4,7 @@ namespace Minesweeper.models
 {
     public class Player : IdentityUser
     {
+        public string? PlayerName { get; set; }
         public int Points { get; set; } = 0;
         public bool IsGuest { get; set; } = false;
     }
